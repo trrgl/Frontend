96362f0705bbcc05330caf57f3d6f9103035dade
Tergel Ganbold <trrgl@Tergels-MacBook-Air.local>
2024-12-18 10:20:22 +0800

    biy daalt

--- pages/project.js
+++ pages/project.js
@@ -112,19 +112,25 @@ export default function Search() {
     );
     
     return (
-        <div className="w-full h-full flex flex-col p-8 items-center bg-[#E0E7FB] space-y-4">
-            <h1 className="text-black font-bold text-2xl">Хэрэглэгчдийн Жагсаалт</h1>
-            <input value={search} onChange={(Search) => setSearch(Search.target.value)} placeholder="Хэрэглэгчийн нэр оруулна уу..." className="w-full h-12 drop-shadow-xl text-black rounded-xl p-2"></input>
+        <div className="w-full h-full flex flex-col p-8 items-center bg-[#1d1e20] space-y-4">
+            <button onClick={() => setGrid(!grid)} className="bg-blue-600 text-white p-2 rounded-md hover:bg-blue-800 absolute right-8 hover:cursor-pointer">{grid ? "Жагсаалт - руу шилжих" : "Grid - руу шилжих"}</button>
+            <h1 className="text-white font-bold text-2xl">Хэрэглэгчдийн Жагсаалт</h1>
+            <input value={search} onChange={(Search) => setSearch(Search.target.value)} placeholder="Хэрэглэгчийн нэр оруулна уу..." className="w-full h-12 drop-shadow-xl placeholder-white bg-[#1f9ea6] rounded-xl p-2"></input>
             <div className={grid == true ? "w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 z-10" : "w-full space-y-4 z-10"}>
-                    {filteredData.map((classmates) => (
-                        <div key={classmates.id} className="w-full bg-white drop-shadow-xl text-black h-full rounded-xl p-4">
+                {filteredData.length > 0 ? (
+                    filteredData.map((classmates) => (
+                        <div key={classmates.id} className="w-full bg-[#1f9ea6] shadow-sm shadow-white text-white h-full rounded-xl p-4 hover:shadow-md hover:shadow-white transition duration-200">
                             <h1 className="font-bold text-2xl">{classmates.name}</h1>
-                        <h1>{classmates.description}</h1>
+                            <h1 className="italic">{classmates.description}</h1>
+                        </div>
+                    ))
+                ) : (
+                    <div className="text-white text-xl absolute text-center left-[37.5%]">
+                        Уучлаарай, Тийм хэрэглэгч олдсонгүй.
                     </div>
-                ))}
+                )}
             </div>
-            <div className="w-screen h-screen bg-[#E0E7FB] absolute -z-10"></div>
-            <div className="text-black text-xl absolute top-36">Уучлаарай, Тийм хэрэглэгч олдсонгүй.</div>
+            <div className="w-screen h-screen bg-[#1d1e20] absolute -z-10"></div>
         </div>
     );
 }
